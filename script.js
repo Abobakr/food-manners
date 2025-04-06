f680031a52222b1ee39c5477bbaf19705e253c1b
Husam Nafrah <husamnafrah@gmail.com>
2025-04-06 15:40:05 +0300

    adding script.js and fav icon files and modifing code for them

---
+++ script.js
@@ -0,0 +1,11 @@
+function hacker_message() {
+    alert("تم تهكير الصفحة")
+}
+function display_hello_message() {
+    console.log('الصفحة قيد التحضير');
+    alert(" 06-أهلا وسهلا ..الصفحة قيد التحضير سيتم تفعيلها بتاريخ 2025-04-\n تاريخ اليوم:" + new Date().toLocaleString())
+}
+function display_my_message(message) {
+   console.log('انت في نفس الصفحة');
+    alert(message)
+}
